2749c706be45a958142ade5e64786ba3bc1967b9
Aravindan Balaji <Aravindan_Balaji@student.uml.edu>
2019-11-25 17:15:39 -0400

    Fixing Issues

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ async function main(){
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://Aravindan:aryayuvimess95@roomiez-ozts7.mongodb.net/test?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://Aravindan:AravSru19@roomiez-ozts7.mongodb.net/test?retryWrites=true&w=majority";
  
 
     const client = new MongoClient(uri, { useUnifiedTopology: true });
